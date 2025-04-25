f197060c3144748b5b36228588c230362eb28833
Dip mondal <work.dipmondal@gmail.com>
2025-04-25 20:55:17 +0600

    update the go

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -16,9 +17,11 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/lucsky/cuid v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mssola/user_agent v0.6.0 // indirect
 	github.com/redis/go-redis/v9 v9.7.3 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/lib/pq"
+	"github.com/mssola/user_agent"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -26,20 +32,6 @@ type URL struct {
 	User        User      `gorm:"foreignKey:UserID"`
 }
 
-// analitics struct for URL clicks
-type Analitics struct {
-	ID                 uint   `gorm:"primaryKey"`
-	ShortCode          string `gorm:"not null"`
-	Click              int    `gorm:"default:0"`
-	CreatedAt          time.Time
-	IPsWhoVisited      pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
-	BrowserWhoVisited  string         `gorm:"type:text"`
-	OSWhoVisited       string         `gorm:"type:text"`
-	DeviceWhoVisited   string         `gorm:"type:text"`
-	ReferrerWhoVisited string         `gorm:"type:text"`
-	LocationWhoVisited string         `gorm:"type:text"`
-}
-
 type User struct {
 	ID        uint   `gorm:"primaryKey"`
 	Username  string `gorm:"uniqueIndex"`
@@ -48,24 +40,34 @@ type User struct {
 	CreatedAt time.Time
 }
 
+// analitics struct for URL clicks
+type Analitics struct {
+	ID                 uint           `gorm:"primaryKey"`
+	ShortCode          string         `gorm:"not null"`
+	Click              int            `gorm:"default:0"`
+	IPsWhoVisited      pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+	BrowserWhoVisited  pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+	OSWhoVisited       pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+	DeviceWhoVisited   pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+	ReferrerWhoVisited pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+	LocationWhoVisited pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+}
+
 type FullURL struct {
 	URL    string `json:"url"`
 	UserID uint   `json:"user_id"` // Added user_id field to the struct
 }
 
-func createShortUrl() string {
-	rand.Seed(time.Now().UnixNano())
-	chars := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	shortURL := ""
-	for i := 0; i < 6; i++ {
-		shortURL += string(chars[rand.Intn(len(chars))])
-	}
-	return shortURL
+type Login struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 var ctx = context.Background()
 var redisClient *redis.Client
 var db *gorm.DB
+var jwtSecret = []byte("kobejmorithiknai")
 
 // Initialize Redis connection
 func initRedis() {
@@ -99,11 +101,75 @@ func initPostgress() {
 	fmt.Println("✅ Connected to PostgreSQL!")
 
 	// Auto migrate URL model
-	err = db.AutoMigrate(&URL{})
+	err = db.AutoMigrate(&User{}, &Analitics{}, &URL{})
 	if err != nil {
 		log.Fatalf("❌ Auto migration failed: %v", err)
 	}
+}
 
+type CustomTokenJwtStucture struct {
+	UID      uint   `json:"userID"`
+	USERNAME string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uint, username string) (string, error) {
+	claims := CustomTokenJwtStucture{
+		UID:      userID,
+		USERNAME: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(240 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func VerifiedToken(tokenString string) (*CustomTokenJwtStucture, error) {
+	claims := &CustomTokenJwtStucture{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("invalid token: %v", err)
+	}
+
+	return claims, nil
+}
+
+func createShortUrl() string {
+	rand.Seed(time.Now().UnixNano())
+
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	length := rand.Intn(4) + 3 // random length between 3 and 6 (3 + [0..3])
+
+	shortURL := make([]byte, length)
+	for i := 0; i < length; i++ {
+		shortURL[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(shortURL)
+}
+
+func GetVisitorInfo(c *fiber.Ctx) (browser, os, device, ip string) {
+	ua := user_agent.New(c.Get("User-Agent"))
+	name, version := ua.Browser()
+	browser = name + " " + version
+	os = ua.OS()
+	if ua.Mobile() {
+		device = "Mobile"
+	} else {
+		device = "Desktop"
+	}
+	ip = c.IP()
+
+	return
 }
 
 func main() {
@@ -111,6 +177,13 @@ func main() {
 
 	initRedis()
 	initPostgress()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3001", // your frontend origin
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: true, // if you're using cookies
+	}))
 
 	// Middleware to log every request
 	app.Use(func(c *fiber.Ctx) error {
@@ -137,11 +210,26 @@ func main() {
 			})
 		}
 
+		fmt.Println(req)
+
 		// Generate short code
 		shortURL := createShortUrl()
-
+		analiticsRecord := Analitics{ShortCode: shortURL}
+		if err := db.Create(&analiticsRecord).Error; err != nil {
+			log.Printf("❌ Failed to store analitics in PostgreSQL: %v", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Failed to save to database",
+				"status":  500,
+			})
+		}
 		// Also store in PostgreSQL for permanent storage
-		urlRecord := URL{ShortCode: shortURL, FullURL: req.URL}
+		urlRecord := URL{
+			ShortCode:   shortURL,
+			FullURL:     req.URL,
+			UserID:      req.UserID, // from request
+			AnaliticsID: analiticsRecord.ID,
+		}
+
 		if err := db.Create(&urlRecord).Error; err != nil {
 			log.Printf("❌ Failed to store in PostgreSQL: %v", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -162,8 +250,56 @@ func main() {
 	})
 
 	// GET: Redirect from shortened URL to full URL
-	app.Get("/r/:code", func(c *fiber.Ctx) error {
+	app.Get("/:code", func(c *fiber.Ctx) error {
 		shortCode := c.Params("code")
+		browser, os, device, _ := GetVisitorInfo(c)
+		ip := c.IP()
+
+		// Get Referer header
+		referer := c.Get("Referer")
+
+		// Default to unknown if missing
+		fromSite := "unknown"
+		if referer != "" {
+			if u, err := url.Parse(referer); err == nil {
+				fromSite = strings.TrimPrefix(u.Hostname(), "www.")
+			}
+		}
+
+		/* type Analitics struct {
+			ID                 uint           `gorm:"primaryKey"`
+			ShortCode          string         `gorm:"not null"`
+			Click              int            `gorm:"default:0"`
+			IPsWhoVisited      pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+			BrowserWhoVisited  pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+			OSWhoVisited       pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+			DeviceWhoVisited   pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+			ReferrerWhoVisited pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+			LocationWhoVisited pq.StringArray `gorm:"type:text[]"` // Use `pq` package for Postgres arrays
+		} */
+
+		var data Analitics
+
+		if err := db.FirstOrCreate(&data, Analitics{ShortCode: shortCode}).Error; err != nil {
+			return err
+		}
+		data.Click++
+		data.BrowserWhoVisited = append(data.BrowserWhoVisited, browser)
+		data.OSWhoVisited = append(data.OSWhoVisited, os)
+		data.DeviceWhoVisited = append(data.DeviceWhoVisited, device)
+		data.ReferrerWhoVisited = append(data.ReferrerWhoVisited, fromSite)
+		data.IPsWhoVisited = append(data.IPsWhoVisited, ip)
+
+		if err := db.Model(&data).Updates(map[string]interface{}{
+			"browser_who_visited":  data.BrowserWhoVisited,
+			"os_who_visited":       data.OSWhoVisited,
+			"device_who_visited":   data.DeviceWhoVisited,
+			"referrer_who_visited": data.ReferrerWhoVisited,
+			"IPsWhoVisited":        data.IPsWhoVisited,
+			"Click":                data.Click,
+		}).Error; err != nil {
+			return err
+		}
 
 		// Check Redis first
 		fullURL, err := redisClient.Get(ctx, shortCode).Result()
@@ -187,6 +323,7 @@ func main() {
 			// Store in Redis for faster future access
 			redisClient.Set(ctx, shortCode, urlRecord.FullURL, 0).Err()
 			fullURL = urlRecord.FullURL
+
 		} else if err != nil {
 			// Redis error
 			log.Printf("❌ Redis GET error: %v", err)
@@ -256,7 +393,8 @@ func main() {
 				"status":  409,
 			})
 		}
-
+		hash, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		user.Password = string(hash)
 		// `Create` the new user
 		if err := db.Create(&user).Error; err != nil {
 			log.Printf("❌ Failed to create user: %v", err)
@@ -265,15 +403,138 @@ func main() {
 				"status":  500,
 			})
 		}
-		c.Cookie(&fiber.Cookie{})
+
+		// Generat token
+
+		token, error := GenerateToken(user.ID, user.Username)
+
+		if error != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Token creation failed"})
+		}
+
+		c.Cookie(&fiber.Cookie{
+			Name:     "token",
+			Value:    token,
+			HTTPOnly: true,
+		})
+
 		return c.JSON(fiber.Map{
 			"message": "User created successfully",
+			"status":  200,
+		})
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		var login Login
+		if err := c.BodyParser(&login); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Invalid request",
+				"status":  400,
+			})
+		}
+
+		fmt.Printf("name %s email %s password %s ", login.Username, login.Email, login.Password)
+
+		if login.Password == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Password  required",
+				"status":  400,
+			})
+		}
+		if login.Username == "" || login.Email == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Username or email are required",
+				"status":  400,
+			})
+		}
+
+		// Check is user is exist for login
+		var user User
+		if err := db.Where("username = ? OR email = ?", login.Username, login.Email).First(&user).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"message": "Invalid username or email",
+					"status":  401,
+				})
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Database error",
+				"status":  500,
+			})
+		}
+
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(login.Password))
+
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Incorrect password",
+				"status":  401,
+			})
+		}
+
+		token, err := GenerateToken(user.ID, user.Username)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Could not generate token",
+				"status":  500,
+			})
+		}
+
+		cookie := fiber.Cookie{
+			Name:     "token",
+			Value:    token,
+			Expires:  time.Now().Add(240 * time.Hour),
+			HTTPOnly: true,
+		}
+
+		c.Cookie(&cookie)
+
+		return c.JSON(fiber.Map{
+			"message": "Login successful",
 			"data": fiber.Map{
-				"id":       user.ID,
-				"username": user.Username,
-				"email":    user.Email,
+				"token": token,
 			},
 			"status": 200,
+		})
+
+	})
+
+	app.Get("/get-all-urls/:id", func(c *fiber.Ctx) error {
+		// Access the user ID from the request params
+		id := c.Params("id")
+
+		// Declare the URL struct and User struct
+		var urls []URL
+		var user User
+
+		// Query for the user
+		if err := db.Where("ID = ?", id).First(&user).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+					"message": "User Not Found!",
+					"status":  404,
+				})
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Server error",
+				"status":  500,
+			})
+		}
+
+		// Query for all URLs associated with the user and preload related Analitics and User
+		if err := db.Preload("Analitics").Preload("User").Where("user_id = ?", user.ID).Find(&urls).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Error fetching URLs",
+				"status":  500,
+			})
+		}
+
+		// Return the list of URLs
+		return c.JSON(fiber.Map{
+			"data":    urls,
+			"message": "URLs found successfully",
+			"status":  200,
 		})
 	})
 
